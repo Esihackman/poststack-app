6ea65e0514914a8bd73a671d7faf4a4e0994d7f6
Esihackman <mabelabenahackman602@gmail.com>
2025-06-17 10:09:51 +0000

    updated the dev branch

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../models/post.model';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
   private baseUrl = environment.apiBaseUrl;
 
-  
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.baseUrl}/posts`);
+  }
  }
