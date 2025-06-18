8917f571167ff2f603759541454680378b72cdc9
Esihackman <mabelabenahackman602@gmail.com>
2025-06-18 02:29:55 +0000

    worked on the delete modal

---
+++ src/app/components/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirm-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './confirm-modal.component.html',
+  styleUrls: ['./confirm-modal.component.scss']
+})
+export class ConfirmModalComponent {
+  @Input() message = 'Are you sure?';
+  @Output() confirmed = new EventEmitter<boolean>();
+
+  confirm() {
+    this.confirmed.emit(true);
+  }
+
+  cancel() {
+    this.confirmed.emit(false);
+  }
+}
--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -3,11 +3,12 @@ import { PostService } from '../../services/post.service';
 import { Post } from '../../models/post.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'post-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ConfirmModalComponent],
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
@@ -15,10 +16,16 @@ export class PostListComponent implements OnInit {
   posts: Post[] = [];
   isLoading = true;
   error: string | null = null;
+  showConfirmModal = false;
+  postToDelete: number | null = null;
 
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.fetchPosts();
+  }
+
+  fetchPosts(): void {
     this.postService.getPosts().subscribe({
       next: (data) => {
         this.posts = data;
@@ -32,17 +39,31 @@ export class PostListComponent implements OnInit {
     });
   }
 
-  deletePost(id: number): void {
-    if (confirm('Are you sure you want to delete this post?')) {
-      this.postService.deletePost(id).subscribe({
+  promptDelete(id: number): void {
+    this.postToDelete = id;
+    this.showConfirmModal = true;
+  }
+
+  handleDeleteConfirm(confirm: boolean): void {
+    if (confirm && this.postToDelete !== null) {
+      this.postService.deletePost(this.postToDelete).subscribe({
         next: () => {
-          this.posts = this.posts.filter(post => post.id !== id);
+          this.posts = this.posts.filter(post => post.id !== this.postToDelete);
+          this.resetModal();
         },
         error: (err) => {
           console.error('Error deleting post:', err);
-          this.error = 'Failed to delete post. Please try again.';
+          this.error = 'Failed to delete post.';
+          this.resetModal();
         }
       });
+    } else {
+      this.resetModal();
     }
+  }
+
+  private resetModal() {
+    this.showConfirmModal = false;
+    this.postToDelete = null;
   }
 }
